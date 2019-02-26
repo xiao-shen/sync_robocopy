82f7838a9b2681ff0dae25eb7691045b89aa8410
xiao-shen <pascalus2000-aecours@yahoo.fr>
2019-02-26 17:54:31 +0100

    Add features
    Add default path which is relative to the script location, with user confirmation and display volume information

--- sync_robo.bat
+++ sync_robo.bat
@@ -1,11 +1,22 @@
 :INIT
 ECHO OFF
+rem Usage: no arguments -> source set to default and destination prompted
+rem        %1           -> default source and %1 is destination
+rem        %1 %2        -> backup from %1 to %2
+rem Update here: https://github.com/xiao-shen/sync_robo
+rem 
+rem Use of variables replaced at execution time (enclosed with exlamation marks instead of percent characters)	
+setlocal EnableDelayedExpansion
 CLS
 ECHO Backup script ****
+ECHO Do not include last backslash in path names please
+ECHO Launched from %~dp0
+ECHO:
 rem This is a comment. 
 
 rem WARNING !
 rem We do not guarantee any possible DATA LOSS. Use this script with attention. Thank you. 
+rem If you misuse this script, you can lose all the data on your PC. 
 rem We recommend you to manually copy your data on an external drive. And disconnect it before testing this script!
 
 rem This script replicates folders from a source (left) to a destination (right).
@@ -13,10 +24,69 @@ rem You can select the subfolders you want to replicate in a file named "sync_ro
 rem The files different from the left side are overwritten on the right side. Deletes are replicated from the left side to the right side. 
 rem So, once again, be careful !
 
-rem Here is the source folder. 
-rem Give the path without last backslash !
-SET "left=D:"
-rem The destination folder will be asked at run time. 
+rem Choice between relative or absolute path
+GOTO RELATIVEPATH
+:ABSOLUTEPATH
+SET "usualSource=D:"
+if [%2]==[] (
+  rem v v v v v v v v v v v v v v v v v v v v v v v v v
+  rem Here is the source folder !!!
+  rem Give the path without last backslash !
+  SET "left=D:"
+  rem      ^^^
+  rem The destination folder will be asked at run time. 
+  rem ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+  rem Set to 1 if you need to confirm the source
+  SET "confirmSource=1"
+  rem Source confirmation
+  if [!left!]==[%usualSource%] (
+    ECHO Copying from '!left!' as usual.
+  ) else (
+    ECHO WARNING: Source was changed from usual to !left! 
+    SET "confirmSource=1"
+  )
+)
+GOTO AFTERPATH
+  
+:RELATIVEPATH
+if [%2]==[] (
+  rem The source folder is determined by the location of the script file
+  rem v v v v v v v v v v v v v v v v v v v v v v v v v
+  rem Here is the source folder, defined relative to the batch file location !!!
+  rem Give the path without last backslash !
+  SET "left=%~dp0..\.."
+  rem            ^^^
+  rem The destination folder will be asked at run time. 
+  rem ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+  rem Set to 1 if you need to confirm the source
+  SET "confirmSource=1"
+  ECHO:
+  ECHO Source set by default to relative path: !left!
+)
+
+:AFTERPATH
+if not [%2]==[] (
+  rem Source from first argument if present
+  SET left=%1
+  ECHO Source set by argument to: !left! 
+  SET "confirmSource=1"
+)
+
+rem Source confirmation by user
+if %confirmSource%==1 (
+  CHOICE /C YN /T 5 /D Y /M "Do you confirm the source folder "
+  IF ERRORLEVEL ==2 (
+    SET /p left= Please give the source folder: 
+  )
+)
+ECHO:
+
+rem Options fot ROBOCOPY
+rem CAUTION  !!!!
+rem This option (/Purge) deletes files on the destination side if they are not in the source side. 
+rem This is useful if you rename or move files. You can remove this option for your tests. 
+SET "opt=/Purge"
+rem  OPT^^^^^^^
 
 rem "sync_robo_list.txt" is the file containing the folder list
 rem Write one folder per line. You can comment a line by using a semicolon (;)
@@ -31,15 +101,25 @@ rem Another log file stores the synchronization screen output.
 SET "log=sync_robo.log"
 
 rem User is prompted the destination folder here (right side). 
-ECHO Do not include last backslash please
-if [%1]==[] (SET /p right= Please give a destination folder: ) else (SET right=%1)
+if [%1]==[] (
+  SET /p right= Please give a destination folder: 
+) else (
+  if [%2]==[] (
+    SET right=%1
+  ) else (
+    SET right=%2
+  )
+  ECHO Destination set by argument to: !right! 
+  CHOICE /C YN /T 5 /D Y /M "Do you confirm the destination folder "
+  IF ERRORLEVEL ==2 (
+    SET /p right= Please give a destination folder: 
+  )
+)
 
-rem Options fot ROBOCOPY
-rem CAUTION: this option (/Purge) deletes files on the destination side if they are not in the source side. 
-rem This is useful if you rename or move files. You can remove this option for your tests. 
-SET "opt=/Purge"
+rem Parses the source's drive letter.
+SET Lletter=%left:~0,1%
 rem Parses the destination's drive letter.
-SET letter=%right:~0,1%
+SET Rletter=%right:~0,1%
 
 rem Some user info.
 :SUMMARY
@@ -48,21 +128,30 @@ ECHO Folders to sync:
 type %fol%
 ECHO:
 ECHO:
-ECHO Left = %left%
-ECHO Right = %right%
+ECHO ----
+ECHO Left Source From = %left%
+rem Give volume information on source and skip a line
+VOL %Lletter%:
+ECHO ----
+ECHO Right Destination To = %right%
 rem Give volume information on destination
-VOL %letter%:
+VOL %Rletter%:
+ECHO ----
 ECHO Options %opt%
 ECHO ----
-CHOICE /C yn /N /M "Do you want to run ROBOCOPY (Y for OK) ?"
+CHOICE /C YN /M "Do you want to run ROBOCOPY " 
 ECHO %ERRORLEVEL%
 IF %ERRORLEVEL%==1 GOTO BACKUP
+ECHO Canceled
 GOTO EOF
+
+
 
 rem The core function. 
 :BACKUP
 ECHO Beginning ...
-echo last sync on %date% at %time%; from %left% to %right%: > %log%
+SET "now=%date%; %time%"
+echo last sync at %now%; from %left% to %right%: > %log%
 ECHO ON
 :LOOP
 rem The loop reads into the file given in %fol and skips lines beginning with ;. 
@@ -76,15 +165,17 @@ robocopy %left%\%%i %right%\%%i /e /r:1 /w:2 /log+:%log% /tee %opt%
 )
 :HIST
 rem LOG file: keep track of the times you ran this script, on the destination and the source. 
-ECHO %left%;%right%; %date%; %time%; >> %right%\%hist%
-ECHO %left%;%right%; %date%; %time%; >> %hist%
+ECHO %left%;%right%; %now%; >> %right%\%hist%
+ECHO %left%;%right%; %now%; >> %hist%
+
+
 
 ECHO OFF
 ECHO:
 ECHO:
 ECHO ---
-ECHO %left% to %right%, %date% %time%
-VOL %letter%:
+ECHO %left% to %right%, %now%
+VOL %Rletter%:
 ECHO Finished. Open %log% to see the screen output again. 
 :END
 PAUSE
